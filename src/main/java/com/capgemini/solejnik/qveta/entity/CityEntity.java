11ae3998fcb2e54dc532cfc6baf736c62625514b
11cervantes11 <SOLEJNIK@CE09507.corp.capgemini.com>
2016-03-23 22:56:43 +0100

    init

---
+++ src/main/java/com/capgemini/solejnik/qveta/entity/CityEntity.java
@@ -0,0 +1,40 @@
+package com.capgemini.solejnik.qveta.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CityEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String name;
+
+	public CityEntity() {
+	}
+
+	public CityEntity(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/test/java/com/capgemini/solejnik/qveta/repository/RepoTest.java
@@ -0,0 +1,30 @@
+package com.capgemini.solejnik.qveta.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.capgemini.solejnik.qveta.entity.CityEntity;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:META-INF/application-context.xml")
+public class RepoTest {
+	@Autowired
+	CityRepository repo;
+
+	@Test
+	public void test() {
+		// given
+		CityEntity city = new CityEntity("Poznan");
+		// when
+		repo.save(city);
+		// then
+		assertNotNull(repo.findAll());
+		assertTrue(repo.findAll().size() > 0);
+	}
+
+}
